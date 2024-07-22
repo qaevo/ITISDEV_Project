17b92c44ed49ce16eb806c18d11fd877478ca1a9
Scervales0259 <80901865+Scervales0259@users.noreply.github.com>
2024-07-22 11:40:36 +0800

    1. Updated parameters in Create Connection
    -port
    -make sure u have updated env file

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const port = 3000;
 // MySQL Connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
